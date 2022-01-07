365051a533f09832b40402e09b96523457cf04e8
vmorenomarin <vmorenomarin@gmail.com>
2022-01-06 22:55:34 -0500

    Added add and list by ID methods for Product controller. Wrote upload function to storage file.

--- backend/src/routes/product.route.js
+++ backend/src/routes/product.route.js
@@ -1,7 +1,10 @@
 const { Router } = require("express");
 const route = Router();
 const productCtrl = require("../controllers/product.controller");
+upload = require("../middlewares/imgUpload");
 
 route.get("/", productCtrl.listProducts);
+route.get("/product/", productCtrl.listProductById);
+route.post("/", upload.single("img"), productCtrl.addProduct);
 
 module.exports = route;
